40ad0d172e924a5fd781da2269c21846c20325a2
Orlov Dmitriy Aleksandrovich <dmorlov@ozon.ru>
2022-07-22 14:24:46 +0300

    Add project with tests and homework

--- src/Ozon.Route256.Postgres.Api/Program.cs
+++ src/Ozon.Route256.Postgres.Api/Program.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Net;
+using FluentMigrator.Runner;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Ozon.Route256.Postgres.Api;
@@ -10,7 +13,7 @@ public static class Program
     public static void Main(string[] args) =>
         CreateHostBuilder(args)
             .Build()
-            .Run();
+            .RunWithMigrate(args);
 
     private static IHostBuilder CreateHostBuilder(string[] args) =>
         Host
@@ -21,4 +24,17 @@ public static class Program
                     builder.UseKestrel(o => o.Listen(IPAddress.Any, 5002, options => options.Protocols = HttpProtocols.Http2));
                     builder.UseStartup<Startup>();
                 });
+
+    private static void RunWithMigrate(this IHost host, string[] args)
+    {
+        if (args.Length > 0 && args[0].Equals("migrate", StringComparison.InvariantCultureIgnoreCase))
+        {
+            using var scope = host.Services.CreateScope();
+            var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+
+            runner.MigrateUp();
+        }
+        else
+            host.Run();
+    }
 }
