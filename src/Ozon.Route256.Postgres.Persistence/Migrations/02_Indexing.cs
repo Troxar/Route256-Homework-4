5acc2cdfaa14c2e88743a8f1ecfcaa76e40e8f2d
Alexander Katkov <afargoth@gmail.com>
2022-08-12 22:51:32 +0700

    [CSHW-4]: Add migration to create index for order_id on order_items

---
+++ src/Ozon.Route256.Postgres.Persistence/Migrations/02_Indexing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentMigrator;
+using Ozon.Route256.Postgres.Persistence.Common;
+
+namespace Ozon.Route256.Postgres.Persistence.Migrations;
+
+[Migration(2, "Add index on order_items")]
+public sealed class IndexForOrderIdOnOrderItems : SqlMigration
+{
+    protected override string GetUpSql(IServiceProvider services) => @"
+CREATE INDEX order_items_order_id_idx ON order_items (order_id);
+";
+
+    protected override string GetDownSql(IServiceProvider services) => @"
+DROP INDEX order_items_order_id_idx;
+";
+}
