faffb77d2d8aa21b7f20f5ea768081af450fb3c8
Alexander Katkov <afargoth@gmail.com>
2022-08-16 23:21:02 +0700

    [CSHW-4]: Add migration to change index on orders

---
+++ src/Ozon.Route256.Postgres.Persistence/Migrations/03_Indexing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FluentMigrator;
+using Ozon.Route256.Postgres.Persistence.Common;
+
+namespace Ozon.Route256.Postgres.Persistence.Migrations;
+
+[Migration(3, "Add index on orders")]
+public sealed class CompoundIndexForOrders : SqlMigration
+{
+    protected override string GetUpSql(IServiceProvider services) => @"
+DROP INDEX orders_client_id_idx;
+CREATE INDEX orders_client_id_order_id_desc_idx ON orders (client_id, order_id DESC);
+";
+
+    protected override string GetDownSql(IServiceProvider services) => @"
+DROP INDEX orders_client_id_order_id_desc_idx;
+CREATE INDEX orders_client_id_idx ON orders (client_id);
+";
+}
